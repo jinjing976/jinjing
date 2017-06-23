3a9295b2cf1c1ed342a772af5b733b5666c8f1fe
jin jing <188888976@qq.com>
2017-06-23 18:03:47 +0800

    test1

---
+++ world/src/main/java/com/trainFramework/world/world.java
@@ -0,0 +1,12 @@
+package com.trainFramework.world;
+
+import com.trainFramework.hello.*;
+
+public class world {
+
+	public static void main(String[] args)
+	{
+		hello h = new hello();
+		h.sayHello("jinjing");
+	}
+}
